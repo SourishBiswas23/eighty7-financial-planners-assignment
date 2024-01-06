0a4f24e8492d149e408811dfdad5cd793efd37c7
SourishBiswas23 <sourish135@gmail.com>
2024-01-06 17:04:15 +0530

    Added the dependencies

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const primaryColor = Color(0xFF08C18A);
+  static const backgroundColor = Color(0xFFE5E7EB);
+  static const foregroundColor = Color(0xFFFFFFFF);
+
+  static const headingBig = 26;
+  static const headingSmall = 22;
+  static const bodyBig = 18;
+  static const bodySmall = 16;
+}
